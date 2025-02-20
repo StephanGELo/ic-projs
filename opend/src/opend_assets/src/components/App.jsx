c0a9f6c8175280b3dd42229dd612fd4089ed68e2
Stephan Lefort <lefortsge@gmail.com>
2025-02-19 22:24:05 -0800

    Use of BrowserRouter, Link, Switch and Route to navigate between different pages in the header component

--- opend/src/opend_assets/src/components/App.jsx
+++ opend/src/opend_assets/src/components/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import homeImage from "../../assets/home-img.png";
+
 import Item from "./Item";
 import Minter from "./Minter";
 
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Minter />
+      {/* <Minter /> */}
       {/* <Item id={NFTId}/> */}
       {/* <img className="bottom-space" src={homeImage} /> */}
       <Footer />
