304b42af08ee532aa3cce342408e9bea22da3434
Stephan Lefort <lefortsge@gmail.com>
2025-02-19 16:12:54 -0800

    Add a loader to show user the crypto is being minted while waiting

--- opend/src/opend_assets/src/components/Minter.jsx
+++ opend/src/opend_assets/src/components/Minter.jsx
@@ -7,8 +7,10 @@ import Item from "./Item";
 function Minter() {
   const { register, handleSubmit } = useForm();
   const [ nftPrincipal, setNFTPrincipal ] = useState("");
+  const [ loaderHidden, setLoaderHidden ] = useState(true);
   
   async function onSubmit(data) {
+    setLoaderHidden(false);
     const name = data.name;
     const image = data.image[0];
     const imageArray = await image.arrayBuffer();
@@ -18,12 +20,18 @@ function Minter() {
     // console.log(newNFTID.toText());
     setNFTPrincipal(newNFTID);
     // console.log("Here in minter nftP is: ", nftPrincipal);
-
+    setLoaderHidden(true);
   }
 
   if (nftPrincipal == "") {
     return (
       <div className="minter-container">
+        <div hidden={loaderHidden} className="lds-ellipsis">
+          <div></div>
+          <div></div>
+          <div></div>
+        <div></div>
+      </div>
         <h3 className="makeStyles-title-99 Typography-h3 form-Typography-gutterBottom">
           Create NFT
         </h3>
